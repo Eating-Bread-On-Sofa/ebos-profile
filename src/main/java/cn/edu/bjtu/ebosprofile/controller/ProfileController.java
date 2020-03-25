5fb02fca230c0e652f6675f647892a93c1c24661
Cypo <voodoo.7@outlook.com>
2020-03-25 19:14:44 +0800

    remove layui

--- src/main/java/cn/edu/bjtu/ebosprofile/controller/ProfileController.java
+++ src/main/java/cn/edu/bjtu/ebosprofile/controller/ProfileController.java
@@ -1,11 +1,9 @@
 package cn.edu.bjtu.ebosprofile.controller;
 
 import cn.edu.bjtu.ebosprofile.service.ProfileService;
-import cn.edu.bjtu.ebosprofile.util.LayuiTableResultUtil;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,12 +15,10 @@ public class ProfileController {
     ProfileService profileService;
     @Autowired
     RestTemplate restTemplate;
-    @Value("${server.edgex}")
-    private String ip;
 
     @CrossOrigin
-    @GetMapping("/list")
-    public LayuiTableResultUtil<JSONArray> getProducts(@RequestParam int page, @RequestParam int limit) {
+    @GetMapping("/{ip}")
+    public JSONArray getProducts(@PathVariable String ip) {
         String url = "http://"+ip+":48081/api/v1/deviceprofile";
         JSONArray products = new JSONArray(restTemplate.getForObject(url,JSONArray.class));
         JSONArray result = new JSONArray();
@@ -32,19 +28,19 @@ public class ProfileController {
             result.add(jo);
         }
         System.out.println("查看所有设备模板"+result);
-        return new LayuiTableResultUtil<>("",result,0,products.size());
+        return result;
     }
 
     @CrossOrigin
-    @GetMapping("/{id}")
-    public JSONObject getThisProduct(@PathVariable String id){
+    @GetMapping("/{ip}/{id}")
+    public JSONObject getThisProduct(@PathVariable String ip,@PathVariable String id){
         String url = "http://"+ip+":48081/api/v1/deviceprofile/"+id;
         return profileService.stamp2Time(restTemplate.getForObject(url,JSONObject.class));
     }
 
     @CrossOrigin
-    @PostMapping("/yml")
-    public String addProduct(@RequestBody String product) {
+    @PostMapping("/{ip}/yml")
+    public String addProduct(@PathVariable String ip,@RequestBody String product) {
         System.out.println("收到\n"+product);
         String url = "http://"+ip+":48081/api/v1/deviceprofile/upload";
         String result = restTemplate.postForObject(url,product,String.class);
@@ -52,8 +48,8 @@ public class ProfileController {
     }
 
     @CrossOrigin
-    @DeleteMapping()
-    public void deleteProduct(@RequestBody String id){
+    @DeleteMapping("/{ip}")
+    public void deleteProduct(@PathVariable String ip,@RequestBody String id){
         String url = "http://"+ip+":48081/api/v1/deviceprofile/id/"+id;
         System.out.println(url);
         restTemplate.delete(url);
