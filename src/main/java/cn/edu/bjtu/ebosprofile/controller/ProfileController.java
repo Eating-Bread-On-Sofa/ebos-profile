1aa633addf34efbcbd107c600b48cb687a8372cf
Cypo <voodoo.7@outlook.com>
2020-06-15 16:29:15 +0800

    do not need name while adding profile to library

--- src/main/java/cn/edu/bjtu/ebosprofile/controller/ProfileController.java
+++ src/main/java/cn/edu/bjtu/ebosprofile/controller/ProfileController.java
@@ -4,15 +4,11 @@ import cn.edu.bjtu.ebosprofile.entity.ProfileYML;
 import cn.edu.bjtu.ebosprofile.service.LogService;
 import cn.edu.bjtu.ebosprofile.service.ProfileService;
 import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
 import io.swagger.annotations.Api;
-import org.apache.kafka.common.protocol.types.Field;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-import org.yaml.snakeyaml.Yaml;
 
-import java.util.Map;
 
 @Api(tags = "设备模板")
 @RequestMapping("/api/profile")
@@ -32,11 +28,9 @@ public class ProfileController {
     }
 
     @CrossOrigin
-    @PostMapping("/name/{name}")
-    public boolean saveRepoProfile(@PathVariable String name, @RequestBody String ymlStr){
-        ProfileYML profileYML = new ProfileYML();
-        profileYML.setName(name);
-        profileYML.setInfo(ymlStr);
+    @PostMapping()
+    public boolean saveRepoProfile( @RequestBody String ymlStr){
+        ProfileYML profileYML = new ProfileYML(ymlStr);
         return profileService.saveYML(profileYML);
     }
 
@@ -94,49 +88,6 @@ public class ProfileController {
         result = profileService.getProfilesName(result,ip);
         return result;
     }
-
-
-//    @ApiImplicitParam(name = "ipset",value = "所有网关的ip，按英文逗号分隔，无空格",required = true,dataTypeClass = String.class)
-//    @CrossOrigin
-//    @GetMapping("/ip")
-//    public JSONArray getAllProducts(@RequestParam String ipset){
-//        JSONArray result = new JSONArray();
-//        String[] ips = ipset.split(",");
-//        for (String ip : ips) {
-//            result = profileService.getProfiles(result,ip);
-//        }
-//        return result;
-//    }
-//
-//
-//    @CrossOrigin
-//    @GetMapping("/ip/{ip}/id/{id}")
-//    public JSONObject getThisProduct(@PathVariable String ip,@PathVariable String id){
-//        String url = "http://"+ip+":48081/api/v1/deviceprofile/"+id;
-//        return profileService.stamp2Time(restTemplate.getForObject(url,JSONObject.class));
-//    }
-//
-//    @CrossOrigin
-//    @PostMapping("/ip/{ip}/yml")
-//    public String addProduct(@PathVariable String ip, @RequestBody String product) {
-//        System.out.println("收到\n" + product);
-//        Yaml yaml = new Yaml();
-//        Map<String, Object> map = (Map<String, Object>) yaml.load(product);
-//        ProfileYML profileYML = new ProfileYML();
-//        String url = "http://" + ip + ":48081/api/v1/deviceprofile/upload";
-//        String result = restTemplate.postForObject(url, product, String.class);
-//        logService.info("向网关" + ip + "添加了新设备模板" + product);
-//        profileYML.setName(result);
-//        profileYML.setInfo(product);
-//        profileService.saveYML(profileYML);
-//        return result;
-//    }
-//
-//    @CrossOrigin
-//    @GetMapping("/yml/{id}")
-//    public String getYML(@PathVariable String id){
-//        return profileService.getYML(id).getInfo();
-//    }
 
     @CrossOrigin
     @GetMapping("/ping")
--- src/main/java/cn/edu/bjtu/ebosprofile/entity/ProfileYML.java
+++ src/main/java/cn/edu/bjtu/ebosprofile/entity/ProfileYML.java
@@ -1,13 +1,25 @@
 package cn.edu.bjtu.ebosprofile.entity;
 
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.yaml.snakeyaml.Yaml;
+
+import java.util.Map;
 
 @Document
 public class ProfileYML {
     @Id
     private String name;
     private String info;
+
+    public ProfileYML(String info) {
+        this.info = info;
+        Yaml yaml = new Yaml();
+        Map<String,Object> map = (Map<String, Object>) yaml.load(info);
+        JSONObject jsonObject = new JSONObject(map);
+        this.name = jsonObject.getString("name");
+    }
 
     public String getName() {
         return name;
