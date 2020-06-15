d5cce2952f2918729910007a2043cc2c33677bc9
Cypo <voodoo.7@outlook.com>
2020-06-15 16:32:44 +0800

    change name of swagger doc

--- src/main/java/cn/edu/bjtu/ebosprofile/controller/ProfileController.java
+++ src/main/java/cn/edu/bjtu/ebosprofile/controller/ProfileController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 
-@Api(tags = "设备模板")
+@Api(tags = "模板管理")
 @RequestMapping("/api/profile")
 @RestController
 public class ProfileController {
